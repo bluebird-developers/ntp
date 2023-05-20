f49214ef57226f6355a7c8e6debe9d692b2d9adb
JULIΛN (Pinguin2001) <75797743+Pinguin2001@users.noreply.github.com>
2023-05-20 21:28:45 +0200

    ntp version 2023.05.20
    Changelog:
    - Initial release

---
+++ script.js
@@ -0,0 +1,78 @@
+function openDialog() {
+  document.getElementById('dialogContainer').style.display = 'block';
+}
+
+function closeDialog() {
+  document.getElementById('dialogContainer').style.display = 'none';
+}
+
+function addShortcut() {
+  var label = document.getElementById('label').value;
+  var url = document.getElementById('url').value;
+
+  if (label && url) {
+    var shortcut = { label: label, url: url };
+
+    var shortcuts = localStorage.getItem('shortcuts');
+    if (!shortcuts) {
+      shortcuts = [];
+    } else {
+      shortcuts = JSON.parse(shortcuts);
+    }
+
+    shortcuts.push(shortcut);
+    if (shortcuts.length > 4) {
+      shortcuts.shift();
+      alert("Error: You cannot add more than 4 shortcuts")
+    }
+
+    localStorage.setItem('shortcuts', JSON.stringify(shortcuts));
+    renderShortcuts();
+
+    closeDialog();
+    document.getElementById('label').value = '';
+    document.getElementById('url').value = '';
+  }
+}
+
+function deleteShortcut(index) {
+  var shortcuts = localStorage.getItem('shortcuts');
+  if (shortcuts) {
+    shortcuts = JSON.parse(shortcuts);
+    shortcuts.splice(index, 1);
+    localStorage.setItem('shortcuts', JSON.stringify(shortcuts));
+    renderShortcuts();
+  }
+}
+
+function renderShortcuts() {
+  var shortcuts = localStorage.getItem('shortcuts');
+  if (shortcuts) {
+    shortcuts = JSON.parse(shortcuts);
+    var container = document.getElementById('shortcuts-container');
+    container.innerHTML = '';
+
+    shortcuts.forEach(function (shortcut, index) {
+      var li = document.createElement('li');
+      li.className = 'shortcut';
+
+      var a = document.createElement('a');
+      a.innerText = shortcut.label;
+      a.href = shortcut.url;
+
+      var deleteButton = document.createElement('button');
+      var deleteButtonIcon = document.createElement('img');
+      deleteButton.className = 'delete-button';
+      deleteButton.onclick = function () {
+        deleteShortcut(index);
+      };
+      deleteButtonIcon.className = 'delete-button-icon';
+      li.appendChild(a);
+      deleteButton.appendChild(deleteButtonIcon);
+      li.appendChild(deleteButton);
+      container.appendChild(li);
+    });
+  }
+}
+
+renderShortcuts();
